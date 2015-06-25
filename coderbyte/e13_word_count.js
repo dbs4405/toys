87acd9ae196446a2de3dfa21256dec75bf26c4b5
David Schiele <david.schiele@gmail.com>
2015-06-25 13:29:50 -0500

    e13 Word count

---
+++ coderbyte/e13_word_count.js
@@ -0,0 +1,8 @@
+function WordCount(str) {
+  var arr = str.split(" ");
+    return arr.length;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+WordCount(readline());
